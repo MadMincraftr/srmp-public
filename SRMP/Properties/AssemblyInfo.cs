288be2fe723f6a41b6d4b2feb06e5e3a6bbd32ab
MadMincraftr <thean.mcgarity@gmail.com>
2023-12-23 14:41:03 -0600

    fix structure

--- SRMP/Properties/AssemblyInfo.cs
+++ SRMP/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("0.0.0.1510")]
-[assembly: AssemblyFileVersion("0.0.0.1510")]
+[assembly: AssemblyVersion("0.0.0.1511")]
+[assembly: AssemblyFileVersion("0.0.0.1511")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
